a8f767412daecb3b3b51d70bdd92587981e57336
desarrollo <celindacorrea@iefincalamesa.edu.co>
2025-05-27 11:16:23 -0500

    barrio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,17 +9,25 @@ public class Main {
     String nombre2="Jhon Castrillon";
     String nombre3="Darley Ramirez Garzón";
     String nombre4="Kevin Stiward Balvin García";
+    String nombre5="Daniel Moreno Casas";
 
     int age1=20;
     int age2=32;
     int age3=27;
     int age4=21;
+    int age5=18;
 
     int id1=1065577990;
     int id2=1152200645;
     int id3=1035439833;
     int id4=1000756221;
+    int id5=1023630753;
 
+    String Barrio1="Barro Blanco";
+    String Barrio2="Zamora";
+    String Barrio3="Mirador Verde";
+    String Barrio4="Manrique";
+    String Barrio5="Villa Paula";
 
         }
     }
