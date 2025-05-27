3e811316433545188a0327471084953750671d4b
desarrollo <celindacorrea@iefincalamesa.edu.co>
2025-05-27 11:04:19 -0500

    feat nombre integrantes

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    String nombre1="Sara Massiel Alvarez Beltran";
+    String nombre2="Jhon Castrillon";
+    String nombre3="Darley Ramirez Garzón";
+    String nombre4="Kevin Stiward Balvin García";
+
+    int age1=20;
+    int age2=32;
+    int age3=27;
+    int age4=21;
+
+    int id1=1065577990;
+    int id2=1152200645;
+    int id3=1035439833;
+    int id4=1000756221;
+
+
+        }
+    }
